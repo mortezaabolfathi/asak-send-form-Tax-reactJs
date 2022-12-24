e6c1db15f599c932982f3ece9f33bcdef940f081
Mansour.vn <pirouz.vn@gmail.com>
2022-12-24 12:12:21 +0330

    refactor: edit Loading component

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,50 +1,12 @@
-import propTypes from "prop-types";
 
-export const Loading  = ({ type }) => {
+export const Loading  = () => {
   return (
-    <>
-      {type === "spiner" ? (
-        <div className="lds-spinner">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      ) : type === "grid" ? (
-        <div className="lds-grid">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      ) : type === "ripple" ? (
-        <div className="lds-ripple">
-          <div></div>
-          <div></div>
-        </div>
-      ) : (
+       
         <div className="lds-facebook">
           <div></div>
           <div></div>
           <div></div>
         </div>
-      )}
-    </>
   );
 };
 
-
-Loading.propTypes = {type:propTypes.oneOf(["spiner", "grid", "ripple"])} 
