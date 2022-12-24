3af5ff5cc97e15bda0b3f015dba6551f1a160309
morteza <morteza.abolfathi73@gmail.com>
2022-12-24 10:26:05 -0800

    feat: add direction page

--- src/pages/UploadExcel/UploadExcel.js
+++ src/pages/UploadExcel/UploadExcel.js
@@ -1,7 +1,17 @@
+import { Button } from "../../components";
+import {useNavigate } from "react-router-dom";
+
 const UploadExcel = () => {
+
+    const navigate=useNavigate()
+
+    const RoutePageHandler = ()=>{
+        navigate("/tableExcel")
+    }
     return ( 
         <div>
             this is Page Upload excel file in dashboard
+            <Button text={"تایید"} Click={()=>RoutePageHandler()}/>
         </div>
      );
 }
--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -1,7 +1,17 @@
+import {Button} from "./../../components";
+import {useNavigate} from "react-router-dom";
+
 const WelcomePage = () => {
+    const navigate=useNavigate();
+
+    const RoutePageHandler = ( ) => {
+        console.log("thi is click here with change page ")
+        navigate("/Login")
+    }
+
     return ( 
         <div>
-            this is welcome page
+            <Button text={"کلید کنید"} Click={()=>RoutePageHandler()}/>
         </div>
      );
 }
