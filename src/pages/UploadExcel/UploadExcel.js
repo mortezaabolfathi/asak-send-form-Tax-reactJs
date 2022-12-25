22617ee649c5ac4eeed9e6db3aa490f1ca622e28
morteza <morteza.abolfathi73@gmail.com>
2022-12-24 17:09:41 -0800

    css: style page upload Excel

--- src/pages/UploadExcel/UploadExcel.js
+++ src/pages/UploadExcel/UploadExcel.js
@@ -1,19 +1,38 @@
 import { Button } from "../../components";
-import {useNavigate } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import { useState, useRef } from "react";
 const UploadExcel = () => {
 
-    const navigate=useNavigate()
+    const navigate = useNavigate()
 
-    const RoutePageHandler = ()=>{
+    const RoutePageHandler = () => {
         navigate("/tableExcel")
     }
-    return ( 
-        <div>
-            this is Page Upload excel file in dashboard
-            <Button text={"تایید"} Click={()=>RoutePageHandler()}/>
+
+    const [files, setFiles] = useState(null);
+
+    const inputRef = useRef();
+    const handleDragOver = (event) => {
+        event.preventDefault();
+    };
+
+    const handleDrop = (event) => {
+        event.preventDefault();
+        setFiles(event.dataTransfer.files)
+    };
+
+
+    return (
+        <div className="uploadExcel">
+            <div className="uploadExcel_text">
+                <h3>لطفا فایل اکسل خود را بارگذاری کنید</h3>
+            </div>
+            <div className="uploadExcel_upload_box">
+                <h3>اینجا فایل اکسل بارگذاری شود</h3>
+            </div>
+            <Button text={"تایید"} size={"normal"} Click={() => RoutePageHandler()} />
         </div>
-     );
+    );
 }
- 
+
 export default UploadExcel;
