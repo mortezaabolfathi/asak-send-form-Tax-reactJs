5930f977ed1e048d1881e33c609fe6f2c7971662
morteza <morteza.abolfathi73@gmail.com>
2022-12-25 09:46:44 -0800

    css: style page taxSetting

--- src/pages/TaxSetting/TaxSetting.js
+++ src/pages/TaxSetting/TaxSetting.js
@@ -1,8 +1,28 @@
+import { Button } from "../../components";
+import { Input } from "../../components";
+
 const TaxSetting = () => {
+
+    const handelTaxSetting = () =>{
+        console.log("accept");
+    }
+
     return ( 
-        <div>
-            this is TaxSetting in dashboard
-        </div>
+        <div className="taxSetting">
+            <div className="taxSetting_text">
+                <h3>تنظیمات مالیاتی</h3>
+            </div>
+            <div className="taxSetting_content">
+                <div className="taxSetting_content_input">
+                    <label>شماره حافظه مالیاتی</label>
+                    <Input />
+                </div>
+                <Button text={"تایید"} size={"normal"} type={"official"}/>
+                <Button text={"دانلود کنید"} size={"normal"} type={"official"}/>
+            </div>
+        
+        <Button text={"فعال سازی"} size={"free"} type={"dark"} Click={() => handelTaxSetting()} />
+    </div>
      );
 }
  
