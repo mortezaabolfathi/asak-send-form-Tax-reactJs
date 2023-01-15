2df7990378ea31b63872e3014ce218fc6dd20850
Mansour.vn <pirouz.vn@gmail.com>
2023-01-15 14:59:25 +0330

    refactor : add user/pass on login page

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -7,13 +7,13 @@ const LoginPage = () => {
   const navigate = useNavigate();
   const successLogin = () => toast.success("ورود با موفقیت انجام شد");
   const failedLogin = () => toast.error("ورود ناموفق، دوباره سعی کنید");
+
   const RoutePageHandler = () => {
     navigate("/dashboard");
     successLogin();
   };
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("klicked");
     const form = new FormData(event.target);
     const data = Object.fromEntries(form);
     console.log(data);
@@ -21,11 +21,11 @@ const LoginPage = () => {
       const response = await Login(data);
       if (response.token) {
         console.log("ورود موفقت آمیز");
-        RoutePageHandler()
+        RoutePageHandler();
       }
     } catch (e) {
       console.log(e);
-      failedLogin()
+      failedLogin();
     }
   };
 
@@ -34,24 +34,32 @@ const LoginPage = () => {
       <div className="loginPage">
         <div className="loginPage__container">
           <h3>لطفا نام کاربری و رمز عبور خودرا وارد کنید</h3>
-            <form id="login"  action={handleSubmit} onSubmit={handleSubmit} className="loginPage__container__inputs">
-              <div className="loginPage__container__inputs__user">
-                <label>نام کاربری :</label>
-                <Input type="text" size="large" name="username" />
-                {/* kminchelle */}
-              </div>
-              <div className="loginPage__container__inputs__pass">
-                <label>پسورد :</label>
-                <Input type="password" size="large" name="password"/>
-                {/* 0lelplR */}
-              </div>
-            </form>
-          <Button
-            text="وارد شوید"
-            type="official"
-            size="normal"
-            form="login"
-          />
+          <form
+            id="login"
+            action={handleSubmit}
+            onSubmit={handleSubmit}
+            className="loginPage__container__inputs"
+          >
+            <div className="loginPage__container__inputs__user">
+              <label>نام کاربری :</label>
+              <Input
+                type="text"
+                size="large"
+                name="username"
+                value="kminchelle"
+              />
+            </div>
+            <div className="loginPage__container__inputs__pass">
+              <label>پسورد :</label>
+              <Input
+                type="password"
+                size="large"
+                name="password"
+                value="0lelplR"
+              />
+            </div>
+          </form>
+          <Button text="وارد شوید" type="official" size="normal" form="login" />
         </div>
       </div>
     </>
